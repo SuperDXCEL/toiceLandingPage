436afe99dbe401960189b6252c841b48848302f0
SuperDXCEL <thedmanmanman@gmail.com>
2026-04-26 16:25:22 +0200

    Configured https through nginx and finished UI

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
+const https = require("https")
 const fs = require("fs")
-const port = 3000
+const port = 3002;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.post("/email", (req, res) => {
   console.log(req.body, " just subscribed")
   fs.appendFileSync("emails.txt", req.body.email.toString() + "\n")
+  res.sendStatus(200);
 });
 
-app.listen(port)
+app.listen(port, '127.0.0.1', () => {
+  console.log("WRAP IT UP")
+});
